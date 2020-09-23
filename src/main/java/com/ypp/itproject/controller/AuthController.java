b1750d3d0d2afb2e0a859000494e7c2da9928020
PaoPaoYue <846260131@qq.com>
2020-09-23 21:45:01 +0800

    fix email validation

--- src/main/java/com/ypp/itproject/controller/AuthController.java
+++ src/main/java/com/ypp/itproject/controller/AuthController.java
@@ -35,6 +35,7 @@ public class AuthController {
     private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
 
     private static final String usernamePattern = "^\\w{3,20}$";
+    private static final String emailPattern = "^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(.[a-zA-Z0-9_-]+)+$";
     private static final String passwordPattern = "^(?=.*[A-Za-z])(?=.*\\d)\\w{8,20}$";
 
     private static final HashFunction hf = Hashing.sha256();
@@ -47,17 +48,20 @@ public class AuthController {
 
     @PostMapping("/register")
     SuccessWapper register(@RequestBody User user) throws RestException {
-        logger.debug(user.toString());
+        logger.debug("new register: " + user.toString());
         String username = user.getUsername();
         String password = user.getPassword();
-        if (username == null || password == null)
-            throw new RestException(0, "no username or password provided");
+        String email = user.getEmail();
+        if (username == null || email == null || password == null)
+            throw new RestException(0, "some fields are empty");
         if (!username.matches(usernamePattern))
-            throw new RestException(1, "username non-valid");
+            throw new RestException(1, "invalid username");
         if (service.getOne(new QueryWrapper<User>().eq("username", user.getUsername())) != null)
             throw new RestException(2, "user already exist");
+        if (!email.matches(emailPattern))
+            throw new RestException(3, "invalid email");
         if (!password.matches(passwordPattern))
-            throw new RestException(3, "password non-valid");
+            throw new RestException(4, "invalid password");
         user.setPassword(hf.hashString(password, Charsets.UTF_8).toString());
         user.setDisplayName(user.getUsername());
         return new SuccessWapper(service.save(user));
@@ -65,7 +69,7 @@ public class AuthController {
 
     @PostMapping("/login")
     Map<String, String> login(@RequestBody User user) throws RestException {
-        logger.debug(user.toString());
+        logger.debug("new login: " + user.toString());
         User actual = service.getOne(new QueryWrapper<User>().eq("username", user.getUsername()));
         if (actual == null)
             throw new RestException(0, "username not found");
